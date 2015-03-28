2cc3382914a6bfb14eb50dcdb87810022fa27b21
li jin <562544599@qq.com>
2015-03-28 13:34:21 +0800

    loginVC [-lj]
    loginVC [-lj]

---
+++ BeautyHouse_iphone/ViewControllers/Login/LoginVC.h
@@ -0,0 +1,13 @@
+//
+//  LoginVC.h
+//  BeautyHouse_iphone
+//
+//  Created by Roy on 15/3/28.
+//  Copyright (c) 2015年 lixiang. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface LoginVC : BaseViewController
+
+@end
---
+++ BeautyHouse_iphone/ViewControllers/Login/LoginVC.m
@@ -0,0 +1,13 @@
+//
+//  LoginVC.m
+//  BeautyHouse_iphone
+//
+//  Created by Roy on 15/3/28.
+//  Copyright (c) 2015年 lixiang. All rights reserved.
+//
+
+#import "LoginVC.h"
+
+@implementation LoginVC
+
+@end
