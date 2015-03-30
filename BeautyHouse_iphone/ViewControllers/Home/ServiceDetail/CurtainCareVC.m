bc6a253de4319722dfc7ca858cf529a21991d00e
lionryin <710523800@qq.com>
2015-03-30 14:44:18 +0800

    [lx]curtainCare prepared Submit

--- BeautyHouse_iphone/ViewControllers/Home/ServiceDetail/CurtainCareVC.m
+++ BeautyHouse_iphone/ViewControllers/Home/ServiceDetail/CurtainCareVC.m
@@ -10,6 +10,13 @@
 
 @interface CurtainCareVC ()
 
+@property (nonatomic) NSInteger curtainNumbers;
+
+- (IBAction)slectCutButtonPressed:(id)sender;
+- (IBAction)slectPlusButtonPressed:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UITextField *curtainNumTF;
+
 @end
 
 @implementation CurtainCareVC
@@ -17,6 +24,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.curtainNumbers = 1;
+    [self resetCurtainNumTFWithCurtainNunbers:self.curtainNumbers];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +33,24 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - 
+- (void)resetCurtainNumTFWithCurtainNunbers:(NSInteger)num{
+    self.curtainNumTF.text = [NSString stringWithFormat:@"%ld",(long)num
+                            ];
 }
-*/
+
+#pragma mark - IBAction
+
+- (IBAction)slectCutButtonPressed:(id)sender {
+    if (self.curtainNumbers > 0) {
+        [self resetCurtainNumTFWithCurtainNunbers:--self.curtainNumbers];
+    }
+    
+}
+
+- (IBAction)slectPlusButtonPressed:(id)sender {
+    [self resetCurtainNumTFWithCurtainNunbers:++self.curtainNumbers];
+}
+
 
 @end
