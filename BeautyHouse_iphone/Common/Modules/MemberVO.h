e31f661e10c5f69acf8b0721dd6e26b243b6c3a9
李进 <562544599@qq.com>
2015-04-25 16:21:33 +0800

    MemberVO
    MemberVO

---
+++ BeautyHouse_iphone/Common/Modules/MemberVO.h
@@ -0,0 +1,13 @@
+//
+//  MemberVO.h
+//  BeautyHouse_iphone
+//
+//  Created by Roy on 15/4/25.
+//  Copyright (c) 2015年 lixiang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MemberVO : NSObject
+
+@end
---
+++ BeautyHouse_iphone/Common/Modules/MemberVO.m
@@ -0,0 +1,13 @@
+//
+//  MemberVO.m
+//  BeautyHouse_iphone
+//
+//  Created by Roy on 15/4/25.
+//  Copyright (c) 2015年 lixiang. All rights reserved.
+//
+
+#import "MemberVO.h"
+
+@implementation MemberVO
+
+@end
